e30e2a9b38fb384283240eb5aa0720bcebacfb0a
Ameen Ahssan <ameen_intern@hexpines.in>
2024-01-16 18:42:57 +0000

    changes by --pug

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ var app = express();
 // view engine setup
 global.__basedir = __dirname;
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
