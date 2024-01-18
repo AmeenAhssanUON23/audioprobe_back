3400f0ae4fcd669e6cabcdd241a0b6a7a28d1cea
Ameen Ahssan <ameen_intern@hexpines.in>
2024-01-18 13:23:41 +0000

    updated availability-controller,router

--- routes/app.routes.js
+++ routes/app.routes.js
@@ -4,16 +4,17 @@ const analysis = require('./analysis/analysis.router');
 const clients = require('./clients/clients.router');
 const appointments = require('./appointments/appointments.router');
 const staff = require('./staff/staff.router');
+const availability = require('./availabilty/availability.router');
 
 
 // API routes --
 function createRoutes(app) {
-    app.use('/api',users,version,analysis,clients,appointments,staff);
+    app.use('/api',users,version,analysis,clients,appointments,staff,availability);
 }
 
 // API routes for testing --
 function createRoutesNoAuth(app) {
-   app.use('/testapi',users,version,analysis,clients,appointments,staff);
+   app.use('/testapi',users,version,analysis,clients,appointments,staff,availability);
 }
 
 module.exports = { createRoutes};
