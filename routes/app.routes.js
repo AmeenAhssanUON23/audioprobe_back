2f4abeb6e8a0a77635b18bf8fa052d385c0dfe70
Ameen Ahssan <ameen_intern@hexpines.in>
2024-01-18 12:21:57 +0000

    updated staff-router

--- routes/app.routes.js
+++ routes/app.routes.js
@@ -3,16 +3,17 @@ const version = require('./appversion/version.router');
 const analysis = require('./analysis/analysis.router');
 const clients = require('./clients/clients.router');
 const appointments = require('./appointments/appointments.router');
+const staff = require('./staff/staff.router');
 
 
 // API routes --
 function createRoutes(app) {
-    app.use('/api',users,version,analysis,clients,appointments);
+    app.use('/api',users,version,analysis,clients,appointments,staff);
 }
 
 // API routes for testing --
 function createRoutesNoAuth(app) {
-   app.use('/testapi',users,version,analysis,clients,appointments);
+   app.use('/testapi',users,version,analysis,clients,appointments,staff);
 }
 
 module.exports = { createRoutes};
